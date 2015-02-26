7e7ccf47d8fb2b5be2656c9994ec505684c07b18
andvintru <andvintru@gmail.com>
2015-02-26 15:15:06 -0800

    Create MainHeader.h

---
+++ MainHeader.h
@@ -0,0 +1,8 @@
+#ifndef MAINHEADER_H_
+#define MAINHEADER_H_
+using namespace std;
+typedef float dollars;
+const dollars BASIC_DUES = 55;
+const dollars PREFERRED_DUES = 95;
+const float PREFERRED_REBATE = 0.06;
+#endif
