61c42aaabedc2bc4760b01becafb67febfeb10f4
andvintru <andvintru@gmail.com>
2015-03-06 00:59:14 -0800

    Update BasicMember.h

--- BasicMember.h
+++ BasicMember.h
@@ -2,6 +2,7 @@
 #define BASICMEMBER_H_
 #include "MainHeader.h"
 #include "Date.h"
+#include "TransactionList.h"
 #include <string>
 #include <iostream>
 #include <iomanip>
@@ -15,39 +16,38 @@ PREFFERED
 class BasicMember
 {
 public:
-
 	BasicMember();
 	virtual ~BasicMember();
 	//Accessors
 	string GetName();
 	int GetMemberNum();
+	int GetMonthEx();
 	Date GetExpirationDate();
 	dollars GetTotalSpent();
-	memberType GetType();
 	BasicMember* GetNext();
 	BasicMember* GetPrev();
-
 	//Mutators
 	void SetName(string newName);
 	void SetMemberNum(int newNum);
 	void SetExpirationDate(Date newDate);
 	void SetTotalSpent(dollars newTotal);
-	void SetType(memberType newType);
 	void SetNext(BasicMember* newNext);
 	void SetPrev(BasicMember* newPrev);
-	virtual void SetAll(string newName, int newNum, int newMonth, int newDay, int newYear, memberType newType, dollars newTotal = 0);
+	virtual void SetAll(string newName, int newNum, int newMonth,
+						int newDay, int newYear, dollars newTotal = 0);
 	virtual void OutputMemberInfo();
-	//void OutputTransactionList();
-	//virtual void AddTransaction(Transaction newTransaction);
+	void OutputTransactionList();
+	virtual void AddTransaction(int newMonth, int newDay,int newYear,
+							string newName,dollars newPrice,int newAmount);
 
 private:
 	string name;
 	int memberNum;
 	Date expirationDate;
 	dollars totalSpent;
-	memberType type;
-	//TransactionList purchaseList;
+	TransactionList purchaseList;
 	BasicMember* next;
 	BasicMember* prev;
 };
 #endif
+
