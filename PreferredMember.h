05dcb4095301c26ce634eb12245f0b1138d18403
andvintru <andvintru@gmail.com>
2015-03-06 01:03:52 -0800

    Update PreferredMember.h

--- PreferredMember.h
+++ PreferredMember.h
@@ -4,15 +4,16 @@
 class PreferredMember : public BasicMember
 {
 public:
-PreferredMember();
-virtual ~PreferredMember();
-virtual void OutputMemberInfo();
-//virtual void AddTransaction(Transaction newTransaction);
-virtual void SetAll(string newName, int newNum, int newMonth, int newDay, int newYear, memberType newType, dollars newTotal = 0, dollars newRebate = 0);
-//Accessor
-dollars GetRebate();
-
+	PreferredMember();
+	virtual ~PreferredMember();
+	virtual void OutputMemberInfo();
+	//virtual void AddTransaction(Transaction newTransaction);
+	virtual void SetAll(string newName, int newNum, int newMonth,
+						int newDay, int newYear, dollars newTotal = 0,
+						dollars newRebate = 0);
+	//Accessor
+	dollars GetRebate();
 private:
-dollars rebate;
+	dollars rebate;
 };
 #endif
