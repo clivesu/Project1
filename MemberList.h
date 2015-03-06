3de46c98ee0645e37866ce6ce4c90488055f2974
andvintru <andvintru@gmail.com>
2015-03-06 01:02:37 -0800

    Update MemberList.h

--- MemberList.h
+++ MemberList.h
@@ -3,21 +3,23 @@
 
 #include "BasicMember.h"
 #include "PreferredMember.h"
-
 class MemberList : public BasicMember
 {
 public:
 	MemberList();
 	~MemberList();
 	BasicMember* FindNode(int memberNum) const;
-	void AddNode(string newName, int newNum, int newMonth, int newDay, int newYear, dollars newTotal);
+	void AddNode(string newName, int newNum, string newType,
+				 int newMonth, int newDay, int newYear, dollars newTotal);
 	void DeleteNode(BasicMember* target);
+	void AddItem(int newMonth,int newDay,int newYear,int newId,
+				 string newName,dollars newPrice,int newAmount);
+	void OutputItemList(int newId);
 	void OutputList();
 	void DeleteList();
-
+	void FindExpire(int);
 private:
-
 	BasicMember* head;
 };
 
-#endif
+#endif /* MEMBERLIST_H_ */
