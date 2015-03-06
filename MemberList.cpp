c401212fa104d3f9d7c13a2654c36ad37d6ac351
andvintru <andvintru@gmail.com>
2015-03-06 01:02:05 -0800

    Update MemberList.cpp

--- MemberList.cpp
+++ MemberList.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <string>
 #include "MemberList.h"
 #include "BasicMember.h"
+#include "PreferredMember.h"
 using namespace std;
 
 MemberList::MemberList()
 {
-head = NULL;
+	head = NULL;
 }
 
 MemberList::~MemberList()
@@ -21,33 +23,133 @@ BasicMember* MemberList::FindNode(int memberNum) const
 	{
 		finder = finder->GetNext();
 	}
+
 	return finder;
 }
 
-void MemberList::AddNode(string newName, int newNum, int newMonth, int newDay, int newYear, dollars newTotal)
+void MemberList::AddNode(string newName, int newNum, string newType,
+		          int newMonth, int newDay, int newYear, dollars newTotal)
 {
 
 	BasicMember* newNode;
+
+	if(newType == "Basic")
+	{
 	newNode = new BasicMember;
-	//the new node should be initialized from input here
 	newNode->SetAll(newName,newNum,newMonth,newDay,newYear,newTotal = 0);
+	}
+
+	if(newType == "Preferred")
+	{
+	newNode = new PreferredMember;
+	newNode->SetAll(newName,newNum,newMonth,newDay,newYear,newTotal = 0);
+	}
 
 	if(head == NULL)
 	{
 		head = newNode;
 	}
+
 	else
 	{
 		BasicMember* current;
 		current = head;
+
 		while(current->GetNext() != NULL)
 		{
 			current = current->GetNext();
 		}
+
 		current->SetNext(newNode);
 		newNode->SetPrev(current);
+		newNode->SetNext(NULL);
 	}
 }
+
+void MemberList::AddItem(int newMonth,int newDay,int newYear,int newId,
+						 string newName,dollars newPrice,int newAmount)
+{
+	BasicMember* current;
+	current    = head;
+	bool found = false;
+
+	while(current != NULL and found == false)
+	{
+		if(current->GetMemberNum() == newId)
+		{
+		current->AddTransaction(newMonth,newDay,newYear,newName,
+								newPrice,newAmount);
+		found = true;
+		}
+		else
+		{
+		current = current->GetNext();
+		}
+	}
+	if(found == false)
+	{
+		cout << "Invaid Member Number!!! " << endl;
+	}
+}
+
+void MemberList::OutputItemList(int newId)
+{
+	BasicMember* current;
+	current    = head;
+	bool found = false;
+
+	while(current != NULL and found == false)
+	{
+		if(current->GetMemberNum() == newId)
+		{
+		current->OutputTransactionList();
+		found = true;
+		}
+		else
+		{
+		current = current->GetNext();
+		}
+	}
+
+	if(found == false)
+	{
+		cout << "Invaid Member Number!!! " << endl;
+	}
+
+}
+
+void MemberList::FindExpire(int month)
+{
+	BasicMember* current;
+	current   = head;
+	bool fact = false;
+
+	while(current != NULL)
+	{
+		if(current->GetMonthEx() == month)
+		{
+			current->OutputMemberInfo();
+			if(typeid(*current) == typeid(BasicMember))
+			{
+				cout << "Renew price: $55.00" << endl;
+			}
+			if(typeid(*current) == typeid(PreferredMember))
+			{
+				cout << "Renew price: $95.00" << endl;
+			}
+			cout << endl;
+			fact = true;
+		}
+		current = current->GetNext();
+	}
+
+	if(fact == false)
+	{
+		cout << "No members expired in this month" << endl;
+	}
+
+}
+
 void MemberList::DeleteNode(BasicMember* target)
 {
 	if(target->GetNext() != NULL)
@@ -72,15 +174,14 @@ void MemberList::OutputList()
 	while(traverser != NULL)
 	{
 		traverser->OutputMemberInfo();
+		cout << endl;
 		traverser = traverser->GetNext();
 	}
 }
 
 void MemberList::DeleteList()
-	{
-
+{
 	BasicMember* deleter;
-
 	while(head!= NULL)
 	{
 		deleter = head->GetNext();
