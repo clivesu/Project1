cb096a2aab4d356ecc0eaba6b97a6fc143e6ca38
andvintru <andvintru@gmail.com>
2015-03-06 01:09:16 -0800

    Update TransactionList.h

--- TransactionList.h
+++ TransactionList.h
@@ -1,32 +1,23 @@
-#ifndef TRANSACTION_H_
-#define TRANSACTION_H_
+#ifndef TRANSACTIONLIST_H_
+#define TRANSACTIONLIST_H_
 
-#include <iostream>
-#include "Date.h"
-using namespace std;
-
-class Transaction
+#include "Transaction.h"
+class TransactionList : public Transaction
 {
 public:
-	Transaction();
-	virtual ~Transaction();
-	virtual void SetItem(int newMonth, int newDay, int newYear, string newName,
-				 dollars newPrice,int newAmount);
-	virtual void PrintItem(); //Outputs the name, number and price of an item
-	Transaction* GetNext();
-	void SetNext(Transaction* newNext);
-	Transaction* GetPrev();
-	void SetPrev(Transaction* newPrev);
+	TransactionList();
+	~TransactionList();
+	void AddNode(int newMonth, int newDay, int newYear,
+				 string newName, dollars newPrice,int newAmount);
+	void DeleteNode(Transaction* target);
+	void OutputList();
+	void DeleteList();
 private:
-	Date    boughtDate;
-	string  itemName;
-	dollars price;
-	int		quanity;
-	Transaction *next;
-	Transaction *prev;
+	Transaction* head;
 };
 
-#endif /* TRANSACTION_H_ */
+#endif /* TRANSACTIONLIST_H_ */
+
 
 
 
