cc2d67c135054dbcc8e6f63f9792b42e4408ab80
Clive <clivesu@gmail.com>
2015-03-09 18:16:22 -0700

    Attempt at merge

--- BasicMember.h
+++ BasicMember.h
@@ -1,8 +1,8 @@
 #ifndef BASICMEMBER_H_
 #define BASICMEMBER_H_
 #include "MainHeader.h"
-#include "Date.h"
 #include "TransactionList.h"
+#include "Date.h"
 #include <string>
 #include <iostream>
 #include <iomanip>
@@ -16,38 +16,47 @@ PREFFERED
 class BasicMember
 {
 public:
+
 	BasicMember();
 	virtual ~BasicMember();
+	BasicMember(const BasicMember& otherMember);
+
 	//Accessors
 	string GetName();
 	int GetMemberNum();
-	int GetMonthEx();
 	Date GetExpirationDate();
 	dollars GetTotalSpent();
+	memberType GetType();
 	BasicMember* GetNext();
 	BasicMember* GetPrev();
+	TransactionList GetPurchaseList();
+
 	//Mutators
 	void SetName(string newName);
 	void SetMemberNum(int newNum);
 	void SetExpirationDate(Date newDate);
 	void SetTotalSpent(dollars newTotal);
+	void SetType(memberType newType);
 	void SetNext(BasicMember* newNext);
 	void SetPrev(BasicMember* newPrev);
-	virtual void SetAll(string newName, int newNum, int newMonth,
-						int newDay, int newYear, dollars newTotal = 0);
+	virtual void SetAll(string newName, int newNum, int newMonth, int newDay, int newYear, memberType newType, dollars newTotal = 0);
 	virtual void OutputMemberInfo();
 	void OutputTransactionList();
-	virtual void AddTransaction(int newMonth, int newDay,int newYear,
-							string newName,dollars newPrice,int newAmount);
+	virtual void AddTransaction(Transaction newTransaction);
+
+	bool RightType();
+
+	virtual dollars GetRebate();
+	virtual void CopyMember(BasicMember otherMember);
 
 private:
 	string name;
 	int memberNum;
 	Date expirationDate;
+	memberType type;
 	dollars totalSpent;
 	TransactionList purchaseList;
 	BasicMember* next;
 	BasicMember* prev;
 };
 #endif
-
--- PreferredMember.cpp
+++ PreferredMember.cpp
@@ -2,32 +2,49 @@
 
 PreferredMember::PreferredMember() : BasicMember(), rebate(0)
 {
-	rebate = 0;
+
 }
 
 PreferredMember::~PreferredMember()
 {
-// does nothing
+
 }
+
+PreferredMember::PreferredMember(const PreferredMember& otherMember)
+{
+
+
+}
+
 void PreferredMember::OutputMemberInfo()
 {
 	BasicMember::OutputMemberInfo();
-	cout << "Rebate Amount: " << rebate << endl;
+	cout << rebate << endl;
 }
-//void PreferredMember::AddTransaction(Transaction newTransaction)
-//{
-// need transaction type
-// Overrides the BasicMember method, but includes rebate
-//}
 
-void PreferredMember::SetAll(string newName, int newNum, int newMonth,
-		     int newDay, int newYear, dollars newTotal, dollars newRebate)
+void PreferredMember::AddTransaction(Transaction newTransaction)
 {
-	BasicMember::SetAll(newName,newNum,newMonth,newDay,newYear,newTotal);
+	BasicMember::AddTransaction(newTransaction);
+
+	rebate = rebate + (newTransaction.GetPrice() * PREFERRED_REBATE);
+
+}
+
+void PreferredMember::SetAll(string newName, int newNum, int newMonth, int newDay, int newYear, memberType newType, dollars newTotal, dollars newRebate)
+{
+	BasicMember::SetAll(newName,newNum,newMonth,newDay,newYear, newType,newTotal);
 	rebate = newRebate;
 }
 
 dollars PreferredMember::GetRebate()
 {
 	return rebate;
+}
+
+void PreferredMember::CopyMember(PreferredMember otherMember)
+{
+	BasicMember::CopyMember(otherMember);
+	rebate = otherMember.GetRebate();
+
+	//Does not alter next and prev
 }
